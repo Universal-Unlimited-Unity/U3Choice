70e35245b97b900d21e427cb4a1de466346df3df
ADAM AAKIF <adamaakif23@gmail.com>
2026-06-28 03:14:43 +0000

    commit

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,7 @@ from pydantic import RedisDsn, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    DATABASE_URL : PostgresDsn 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
