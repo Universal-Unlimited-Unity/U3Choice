08b62128d6db9c2201e2fc4071013b23a3352b32
ADAM AAKIF <adamaakif23@gmail.com>
2026-06-29 01:23:31 +0000

    structure done!

---
+++ backend/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, EmailStr
+from datetime import date
+from uuid import uuid4, UUID
+class user(BaseModel):
+    id: UUID = Field(default_factory=uuid4())
+    username: str
+    email: EmailStr
+    pwd_hash: str
+    name: str
+    bio: str | None = Field(max_length=50)
+    photo_url: str
+    dob: date
+    status: str
+    created_at: date
+    updated_at: date
+    last_login: date
+    verified: bool = False
+
