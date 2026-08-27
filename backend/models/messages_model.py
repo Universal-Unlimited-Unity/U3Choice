4fa9b1c3bad70d8247f816cd69fbaeb414a992aa
adam aakif <adamaakif23@gmail.co>
2026-08-27 15:01:12 +0100

    commit

--- backend/models/messages_model.py
+++ backend/models/messages_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, UUID,DateTime, Table, Column, Index, String
 from database import metadata
 from .user_model import users_table
+
 messages_table = Table(
     "messages",
     metadata,
